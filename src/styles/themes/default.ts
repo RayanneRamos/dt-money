33a79f8a885c3995089fa80f77fbfc0b4c871492
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-03-27 14:45:54 -0300

    criando o theme default da aplicação

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  white: '#fff',
+
+  'gray-100': '#e1e1e6',
+  'gray-300': '#c4c4c4',
+  'gray-400': '#8d8d99',
+  'gray-500': '#7c7c8a',
+  'gray-600': '#323238',
+  'gray-700': '#29292e',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'green-300': '#00d37e',
+  'green-500': '#00875f',
+  'green-700': '#015f43',
+
+  'red-300': '#f75a68',
+  'red-500': '#ab222e',
+  'red-700': '#7a1921',
+} as const
