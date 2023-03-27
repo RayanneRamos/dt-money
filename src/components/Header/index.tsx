4e6ae9d5b11e1ed8371cceb895ac7aa559721253
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-03-27 17:16:12 -0300

    importando o componente NewTransactionModal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImage from '../../assets/logo.svg';
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImage} alt='DT Money' />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
