c4282591f93df6c9b547f2b098f99606d6e3bfde
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-03-27 15:45:45 -0300

    criando o componente Table da página de Transactions

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,34 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>12/04/2022</td>
+            </tr>
+            <tr>
+              <td width='50%'>Hambúrger</td>
+              <td>
+                <PriceHighLight variant="outcome">-R$ 59,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
+}
