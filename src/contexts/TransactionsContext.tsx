6c4e82254ad903ce096f7ceb459c3b4de626d5cf
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-03-28 17:14:30 -0300

    criando uma nova transação

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,9 +10,18 @@ interface Transaction {
   createdAt: string;
 }
 
+interface CreateTransactionInput {
+  description: string;
+  price: number;
+  category: string;
+  type:'income' | 'outcome';
+}
+
+
 interface TransactionsContextType {
   transactions: Transaction[];
   fetchTransactions: (query?: string) => Promise<void>;
+  createTransaction: (data:  CreateTransactionInput) => Promise<void>;
 }
 
 interface TransactionsProviderProps {
@@ -36,12 +45,29 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     setTransactions(response.data);
   }
 
+  async function createTransaction(data: CreateTransactionInput) {
+    const { description, price, category, type } = data;
+    
+    const response = await api.post('transactions', {
+      description,
+      price,
+      category,
+      type,
+      createdAt: new Date(),
+    });
+
+    setTransactions(state => [
+      response.data,
+      ...state,
+    ])
+  }
+
   useEffect(() => {
     fetchTransactions();
   }, []);
 
   return (
-    <TransactionsContext.Provider value={{ transactions, fetchTransactions }} >
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions, createTransaction }} >
       {children}
     </TransactionsContext.Provider>
   )
