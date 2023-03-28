329d086631d2bfcdfea9a0cde8b8190631d5884d
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-03-28 15:21:47 -0300

    calculando os resumos de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,32 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
-interface Transaction {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  price: number;
-  category: string;
-  createdAt: string;
-}
 
-export function Transactions() {
-  const [ transactions, setTransactions ] = useState<Transaction[]>([]);
-  
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3000/transactions');
-    const data = await response.json();
 
-    setTransactions(data);
-  }
+export function Transactions() {  
+  const { transactions } = useContext(TransactionsContext);
 
-  useEffect(() => {
-    loadTransactions();
-  }, []);
-  
   return (
     <div>
       <Header />
