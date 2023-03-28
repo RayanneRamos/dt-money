632ce3873337d496d94cca8392be66d2c9656fd3
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-03-28 16:51:34 -0300

    fazendo a busca por parâmetros

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { MagnifyingGlass } from 'phosphor-react';
 import { useForm } from "react-hook-form";
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useContext } from "react";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -12,12 +14,13 @@ type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext);
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInputs>({
     resolver: zodResolver(searchFormSchema),
   });
 
   async function handleSearchTransactions(data: SearchFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    await fetchTransactions(data.query);
     
     console.log(data);
   }
