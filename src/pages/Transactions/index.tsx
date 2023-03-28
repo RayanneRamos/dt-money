bd76410d479eef3ebbd1425892ae3ac3487f1e78
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-03-28 15:43:15 -0300

    formatando as datas e os preços

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
@@ -23,10 +24,13 @@ export function Transactions() {
                 <tr key={transaction.id}>
                   <td width='50%'>{transaction.description}</td>
                   <td>
-                    <PriceHighLight variant={transaction.type}>{transaction.price}</PriceHighLight>
+                    <PriceHighLight variant={transaction.type}>
+                      { transaction.type === 'outcome' && '- ' }
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighLight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               )
             }) }
